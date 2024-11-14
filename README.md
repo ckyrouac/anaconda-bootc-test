b6b872f77bceefb8650ae3d89690d5700e757a30
Chris Kyrouac <ckyrouac@redhat.com>
2024-11-14 17:06:07 -0500

    A little easier for other people to use

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 This is an experimental repo for testing bootc LBIs with anaconda. It has some helpers for setting up a dev/test environment.
 
-First download the Fedora 41 network install ISO into the resources directory. Then, use `create.sh` to spin up a VM via ks.cfg and `delete.sh` to remove it.
+First download the Fedora 41 network install ISO into the resources directory. Modify `create.sh` to point to your local copies of bootc and rpm-ostree. Then, use `create.sh` to spin up a VM via ks.cfg and `delete.sh` to remove it.
+
+WIP code:
+[bootc](https://github.com/ckyrouac/bootc/tree/no-container-install)
+[rpm-ostree](https://github.com/ckyrouac/rpm-ostree/tree/anaconda-test)
+[ostree-rs-ext](https://github.com/ckyrouac/ostree-rs-ext/tree/anaconda-test)
--- create.sh
+++ create.sh
@@ -2,6 +2,8 @@
 
 # modify ostree-rs-ext
 # override ostree-rs-ext dependency in rpm-ostree/Cargo.toml to point to local dir
+# build bootc via make
+# copy bootc binary into  resources/bootc.cfg
 # build rpm-ostre via make
 # copy rpm-ostree binary into  resources/rpm-ostree.cfg because
 # for some reason initrd-inject only persists files with a .cfg extension?
@@ -9,15 +11,19 @@
 set -e
 set -x
 
-cd /home/chris/projects/bootc || exit
-make
-cp ./target/release/bootc /home/chris/projects/anaconda-bootc-test/resources/bootc.cfg
+BOOTC_REPO=/home/chris/projects/bootc
+RPM_OSTREE_REPO=/home/chris/projects/rpm-ostree
+ANACONDA_BOOTC_TEST_REPO=/home/chris/projects/anaconda-bootc-test
 
-cd /home/chris/projects/rpm-ostree || exit
+cd "$BOOTC_REPO" || exit
 make
-cp ./target/release/rpm-ostree /home/chris/projects/anaconda-bootc-test/resources/rpm-ostree.cfg
+cp ./target/release/bootc "${ANACONDA_BOOTC_TEST_REPO}/resources/bootc.cfg"
 
-cd /home/chris/projects/anaconda-bootc-test || exit
+cd "$RPM_OSTREE_REPO" || exit
+make
+cp ./target/release/rpm-ostree "${ANACONDA_BOOTC_TEST_REPO}/resources/rpm-ostree.cfg"
+
+cd "$ANACONDA_BOOTC_TEST_REPO" || exit
 
 virt-install \
 --name anaconda-bootc-test \
